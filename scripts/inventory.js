36811ba6b3c770b436954b1a226396943dd7fc1e
Bee-Jay Warfield <itsabeejay@yahoo.com>
2018-12-27 04:39:31 -0800

    Starting

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,3 +1,15 @@
 (function(){
+    document.getElementById("xlfile").addEventListener('change', function(e){
+        
+        let reader = new FileReader();
+
+        reader.onload = function(e){
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {type : 'binary'});
+
+        }
+        reader.readAsBinaryString(f);
+    }, false);
+
 
 })();
